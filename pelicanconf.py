b5d2b4a7ee0166cee43d7bc5e1e1ca32b6e9b577
geomars <egogeomars@gmail.com>
2017-05-07 08:39:31 +0700

    Edit configuration

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,6 +12,49 @@ TIMEZONE = 'Asia/Jakarta'
 
 DEFAULT_LANG = u'en'
 
+# Path-specific metadata
+EXTRA_PATH_METADATA = {
+    'extra/robots.txt': {'path': 'robots.txt'},
+    }
+
+# Static path    
+STATIC_PATHS = ['pages', 'extra', 'images', 'pdfs', 'downloads']
+
+# Posts settings
+ARTICLE_PATHS = ['pages']
+
+SLUGIFY_SOURCE = 'title' #automatically slugify title
+
+ARTICLE_URL = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}.html'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}.html'
+
+PAGE_URL = 'pages/{slug}.html'
+PAGE_SAVE_AS = 'pages/{slug}.html'
+
+CATEGORY_URL = 'category/{slug}.html'
+CATEGORY_SAVE_AS = 'category/{slug}.html'
+
+TAG_URL = 'tag/{slug}.html'
+TAG_SAVE_AS = 'tag/{slug}.html'
+
+AUTHOR_URL = 'author/{slug}.html'
+AUTHOR_SAVE_AS = 'author/{slug}.html'
+
+YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/index.html'
+DAY_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/{date:%d}/index.html'
+
+# Themes
+THEME = 'themes/pelican-striped-html5up'
+
+# Plugins
+PLUGIN_PATHS = ['plugins']
+PLUGINS = ['neighbors', 'assets', 'sitemap', 'extract_toc', 'gravatar']
+
+SITEMAP_SAVE_AS = 'sitemap.xml'
+
+DIRECT_TEMPLATES = ['index', 'categories', 'authors', 'archives']
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -33,3 +76,13 @@ DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Markdown extensions:
+MARKDOWN = {
+    'extension_configs': {
+        'markdown.extensions.codehilite': {'css_class': 'highlight'},
+        'markdown.extensions.extra': {},
+        'markdown.extensions.meta': {},
+    },
+    'output_format': 'html5',
+}
